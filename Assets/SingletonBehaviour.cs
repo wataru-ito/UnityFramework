e96ca8593c396de492063fcfd1c82e572dccd38b
Wataru_Ito <true_black@msn.com>
2017-06-24 20:58:53 +0900

    シングルトンユーティリティ追加

---
+++ Assets/SingletonBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour 
+	where T : MonoBehaviour
+{
+	protected static T s_instance;
+
+	public static T instance
+	{
+		get
+		{
+			if (s_instance == null)
+				s_instance = Object.FindObjectOfType<T>();
+			return s_instance;
+		}
+	}
+
+	public static bool Exists()
+	{
+		return s_instance != null;
+	}
+
+	//------------------------------------------------------
+	// unity system functions
+	//------------------------------------------------------
+
+	protected virtual void Awake()
+	{
+		if (s_instance)
+		{
+			Debug.LogWarningFormat("{0} already exists.", typeof(T).FullName);
+		}
+
+		s_instance = this as T;
+	}
+
+	protected virtual void OnDestory()
+	{
+		if (s_instance == this)
+		{
+			s_instance = null;
+		}
+	}
+}
