28b46c86eb2345f82b2d3cef746d45f6a19d60c0
Wataru_Ito <true_black@msn.com>
2017-07-02 14:32:00 +0900

    カスタムGUIScope追加

---
+++ Assets/Editor/GUIScope/ColorScope.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ColorScope : GUI.Scope
+{
+	readonly Color m_prev;
+
+	public ColorScope(Color color)
+	{
+		m_prev = GUI.color;
+		GUI.color = color;
+	}
+
+	protected override void CloseScope()
+	{
+		GUI.color = m_prev;
+	}
+}
---
+++ Assets/Editor/GUIScope/IndentScope.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class IndentScope : GUI.Scope
+{
+	public IndentScope()
+	{
+		++EditorGUI.indentLevel;
+	}
+
+	protected override void CloseScope()
+	{
+		--EditorGUI.indentLevel;
+	}
+}
