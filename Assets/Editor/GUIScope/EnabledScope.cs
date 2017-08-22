b353af07255392eb3a3761bc83f4ed7414b67e55
Wataru_Ito <true_black@msn.com>
2017-08-22 23:46:48 +0900

    GUI.enabledを制御するスコープ追加

---
+++ Assets/Editor/GUIScope/EnabledScope.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EnabledScope : GUI.Scope
+{
+	bool m_prev;
+
+	public EnabledScope(bool enabled)
+	{
+		m_prev = GUI.enabled;
+		GUI.enabled = enabled;
+	}
+
+	protected override void CloseScope()
+	{
+		GUI.enabled = m_prev;
+	}
+}
