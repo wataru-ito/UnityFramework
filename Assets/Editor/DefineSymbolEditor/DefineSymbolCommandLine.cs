bf7abe4cbd0e1b649bd4d975f70fb8683ca8369b
wataru-ito <true_black@msn.com>
2017-08-28 01:59:13 +0900

    上げ忘れ

--- Assets/Editor/DefineSymbolEditor/DefineSymbolCommandLine.cs
+++ Assets/Editor/DefineSymbolEditor/DefineSymbolCommandLine.cs
@@ -13,12 +13,7 @@ namespace DefineSymbolEditor
 		public static string GetScriptDefineSymbol(string presetName, BuildTargetGroup targetGroup)
 		{
 			var data = DefineSymbolData.Load();
-
-			var preset = data.presets.Find(i => i.name == presetName);
-			if (preset == null) return null;
-
-			var index = Array.FindIndex(preset.symbols, i => i.target == targetGroup);
-			return index >= 0 ? preset.symbols[index].symbol : null;
+			return data.GetPresetSymbols(presetName, targetGroup);
 		}
 	}
 }
