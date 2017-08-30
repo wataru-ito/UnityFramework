e8ac62e330e313183e8715a6f165b98c6b4f8ac3
wataru-ito <true_black@msn.com>
2017-08-30 22:01:41 +0900

    Backボタン使ってゲームから抜ける

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Framework.Game
+{
+	public class GameManager : MonoBehaviour
+	{
+		[SerializeField, SceneName] string m_backSceneName;
+
+		//------------------------------------------------------
+		// event
+		//------------------------------------------------------
+		
+		public void OnBackButtonDown()
+		{
+			SceneManager.LoadScene(m_backSceneName);
+		}
+	}
+}
---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Framework.Title
+{
+	public class TitleManager : MonoBehaviour
+	{
+		[SerializeField, SceneName] string m_nextSceneName;
+
+		//------------------------------------------------------
+		// event
+		//------------------------------------------------------
+
+		public void OnStartButtonDown()
+		{
+			SceneManager.LoadScene(m_nextSceneName);
+		}
+	}
+}
