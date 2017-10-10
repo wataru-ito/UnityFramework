7717797f24d087c911d40733c697b4f264594c51
wataru-ito <true_black@msn.com>
2017-10-10 22:21:15 +0900

    LockInspector＆ContainClip追加

---
+++ Assets/Plugins/Editor/CustomInspectors/AnimatorControllerInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+
+[CustomEditor(typeof(AnimatorController))]
+public class AnimatorControllerInspector : Editor
+{
+	//------------------------------------------------------
+	// unity system function
+	//------------------------------------------------------
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		EditorGUILayout.Space();
+
+		if (GUILayout.Button("CombineAnimationclip"))
+		{
+			ContainClip.Create(target as AnimatorController);
+		}
+	}
+}
