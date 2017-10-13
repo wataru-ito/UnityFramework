3c6afecba5a763a2dd9eb4a519e57582ac9a078e
wataru-ito <true_black@msn.com>
2017-10-13 22:00:53 +0900

    フェーダー作成

---
+++ Assets/Scripts/System/VFX/Fader.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Framework.VFX
+{
+	[RequireComponent(typeof(Camera))]
+	public class Fader : SingletonBehaviour<Fader>
+	{
+		const string kWhiteKeyword = "WHITE";
+
+		public enum ColorType
+		{
+			White,
+			Black,
+		}
+
+		[SerializeField] Material m_material;
+		[SerializeField] ColorType m_colorType;
+		[SerializeField, Range(0,1)] float m_thickness;
+
+		Material m_materialInstance;
+		int m_thicknessId;
+
+		Coroutine m_coroutine;
+
+
+		//------------------------------------------------------
+		// unity system function
+		//------------------------------------------------------
+
+		protected override void Awake()
+		{
+			base.Awake();
+			
+			m_materialInstance = Instantiate<Material>(m_material);
+			m_thicknessId = Shader.PropertyToID("_Thickness");
+			SetColor(m_colorType);
+		}
+
+		void OnDisable()
+		{
+			StopIntepolate();
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+
+			if (m_materialInstance)
+			{
+				Destroy(m_materialInstance);
+					m_materialInstance = null;
+			}
+		}
+
+		void OnRenderImage(RenderTexture src, RenderTexture dest)
+		{
+			Graphics.Blit(src, dest, m_materialInstance);
+		}
+
+
+		//------------------------------------------------------
+		// 色
+		//------------------------------------------------------
+
+		public ColorType colorType
+		{
+			get { return m_colorType; }
+			set
+			{
+				if (m_colorType != value)
+				{
+					SetColor(value);
+					SetThickness(m_thickness);
+				}
+			}
+		}
+
+		void SetColor(ColorType colorType)
+		{
+			m_colorType = colorType;
+			if (m_colorType == ColorType.White)
+			{
+				m_materialInstance.EnableKeyword(kWhiteKeyword);
+			}
+			else
+			{
+				m_materialInstance.DisableKeyword(kWhiteKeyword);
+			}
+		}
+
+
+		//------------------------------------------------------
+		// 強さ
+		//------------------------------------------------------
+
+		public float thickness
+		{
+			get { return m_thickness; }
+			set
+			{
+				var _value = Mathf.Clamp01(value);
+				if (!Mathf.Approximately(m_thickness, _value))
+				{
+					StopIntepolate();
+					SetThickness(_value);
+				}
+			}
+		}
+
+		public void SetThickness(float value, float duration)
+		{
+			StopIntepolate();
+			m_coroutine = StartCoroutine(yInterpolate(m_thickness, value, duration));
+		}
+
+		void SetThickness(float value)
+		{
+			m_thickness = value;
+
+			// シェーダーで１つの引き算を削減するためにここで計算
+			m_materialInstance.SetFloat(m_thicknessId, 
+				m_colorType == ColorType.White ? value : 1 - value);
+		}
+
+		void StopIntepolate()
+		{
+			if (m_coroutine != null)
+			{
+				StopCoroutine(m_coroutine);
+				m_coroutine = null;
+			}
+		}
+		
+		IEnumerator yInterpolate(float from, float to, float duration)
+		{
+			float t = 0;
+			while (t < duration)
+			{
+				t += Time.deltaTime;
+				SetThickness(Mathf.Lerp(from, to, t / duration));
+				yield return null;
+			}
+		}
+
+
+		//------------------------------------------------------
+		// editor
+		//------------------------------------------------------
+#if UNITY_EDITOR
+
+		void OnValidate()
+		{
+			if (m_materialInstance)
+			{
+				SetColor(m_colorType);
+				SetThickness(m_thickness);
+			}			
+		}
+
+		#endif
+	}
+}
---
+++ Assets/Shaders/VFX/Fader.shader
@@ -0,0 +1,58 @@
+﻿Shader "Framework/VFX/Fader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Thickness ("Thickness", Range(0,1)) = 0
+	}
+	
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma shader_feature WHITE
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			float _Thickness;
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				#if WHITE
+				return col + (1 - col) * _Thickness;
+				#else
+				return col * _Thickness;
+				#endif
+			}
+			ENDCG
+		}
+	}
+}
