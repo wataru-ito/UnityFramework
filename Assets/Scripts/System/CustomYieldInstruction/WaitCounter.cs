0585b8a3b57b7e2750484a22852661e67ddb86e7
wataru-ito <true_black@msn.com>
2017-10-16 21:37:30 +0900

    WaitCounter追加

---
+++ Assets/Scripts/System/CustomYieldInstruction/WaitCounter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using ReferenceClearCallback = System.Action;
+
+/// <summary>
+/// 内部に参照カウンタを持ち、参照が０になるまで待ち続ける
+/// </summary>
+public class WaitCounter : CustomYieldInstruction
+{
+	int m_refCount;
+	ReferenceClearCallback m_callback;
+
+	//------------------------------------------------------
+	// lifetime
+	//------------------------------------------------------
+
+	public WaitCounter(ReferenceClearCallback callback = null)
+	{
+		m_callback = callback;
+	}
+
+	//------------------------------------------------------
+	// accessor
+	//------------------------------------------------------
+
+	public void AddRef()
+	{
+		++m_refCount;
+	}
+
+	public void RemoveRef()
+	{
+		Assert.IsTrue(m_refCount > 0, "RefCount remove over.");
+		if (--m_refCount == 0 && m_callback != null)
+		{
+			m_callback();
+			m_callback = null;
+		}
+	}
+
+	public override bool keepWaiting
+	{
+		get { return m_refCount > 0; }
+	}
+}
