da93aa1d749bb90f3ae20f39033a3b04cee0ff33
wataru-ito <true_black@msn.com>
2017-10-18 21:33:52 +0900

    冷静に考えたら内部クラス不要だった

--- Assets/Plugins/Editor/EditorCoroutine/EditorCoroutine.cs
+++ Assets/Plugins/Editor/EditorCoroutine/EditorCoroutine.cs
@@ -5,48 +5,59 @@ using UnityEditor;
 
 namespace Framework
 {
-	public class EditorCoroutine
+	public class EditorCoroutine : CustomYieldInstruction
 	{
-		private class Instance : CustomYieldInstruction
-		{
-			IEnumerator m_enumerator;
-			CustomYieldInstruction m_current;
+		IEnumerator m_enumerator;
+		CustomYieldInstruction m_current;
 
-			public Instance(IEnumerator enumerator)
-			{
-				m_enumerator = enumerator;
-				EditorApplication.update += Update;
-			}
-
-			void Update()
-			{
-				if (m_current != null && m_current.keepWaiting)
-					return;
-
-				if (m_enumerator.MoveNext())
-				{
-					m_current = m_enumerator.Current as CustomYieldInstruction;
-					return;
-				}
-				
-				m_enumerator = null;
-				m_current = null;
-				EditorApplication.update -= Update;
-			}
-
-			public override bool keepWaiting
-			{
-				get { return m_enumerator != null; }
-			}
-		}
 
 		//------------------------------------------------------
 		// static function
 		//------------------------------------------------------
 
-		public static CustomYieldInstruction Run(IEnumerator enumerator)
+		/// <summary>
+		/// わざわざファクトリにする必要はないんだけど、インスタンス生成して起動するのも気持ち悪いので。
+		/// </summary>
+		public static EditorCoroutine Run(IEnumerator enumerator)
 		{
-			return new Instance(enumerator);
+			return new EditorCoroutine(enumerator);
+		}
+		
+
+		//------------------------------------------------------
+		// lifetime
+		//------------------------------------------------------
+
+		private EditorCoroutine(IEnumerator enumerator)
+		{
+			m_enumerator = enumerator;
+			EditorApplication.update += Update;
+		}
+
+
+		//------------------------------------------------------
+		// update
+		//------------------------------------------------------
+
+		void Update()
+		{
+			if (m_current != null && m_current.keepWaiting)
+				return;
+
+			if (m_enumerator.MoveNext())
+			{
+				m_current = m_enumerator.Current as CustomYieldInstruction;
+				return;
+			}
+				
+			m_enumerator = null;
+			m_current = null;
+			EditorApplication.update -= Update;
+		}
+
+		public override bool keepWaiting
+		{
+			get { return m_enumerator != null; }
 		}
 	}
 }
