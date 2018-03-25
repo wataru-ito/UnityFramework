641795e097680c8cbfec0e66ff60bc04c7bb95f1
wataru-ito <true_black@msn.com>
2018-03-25 11:13:49 +0900

    feat:EditorGUIUtility.labelWidthを制御するGUIScope追加

---
+++ Assets/Amber/Editor/GUIScopes/LabelWidthScope.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Amber
+{
+	public sealed class LabelWidthScope : GUI.Scope
+	{
+		readonly float m_labelWidth;
+
+		public LabelWidthScope(float labelWidth)
+		{
+			m_labelWidth = EditorGUIUtility.labelWidth;
+			EditorGUIUtility.labelWidth = labelWidth;
+		}
+
+		protected override void CloseScope()
+		{
+			EditorGUIUtility.labelWidth = m_labelWidth;
+		}
+	}
+}
