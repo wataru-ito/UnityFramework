3327159bbc16f66ac2eed361d35cafac1ebd1d7a
wataru-ito <true_black@msn.com>
2018-03-25 11:49:47 +0900

    feat:Amberのマニュアル作成開始

---
+++ Assets/Amber/Editor/AmberManualLink.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Amber
+{
+	class AmberManualLink
+	{
+		[MenuItem("Help/Amber/マニュアル", false, -1)]
+        static void OpenManual()
+		{
+			Application.OpenURL(Application.dataPath + "/../Manual/AmberManual.html");
+		}	
+	}
+}
